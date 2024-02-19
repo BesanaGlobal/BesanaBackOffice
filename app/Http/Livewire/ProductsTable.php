91fa6c5043b7bf5c5d11fd92e1a557a22df4122b
Jorge Contreras <jluiscontreras95@gmail.com>
2024-02-19 19:14:48 +0100

    [ft][controller][productDataTable]

--- app/Http/Livewire/ProductsTable.php
+++ app/Http/Livewire/ProductsTable.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Columns\ButtonGroupColumn;
+use Rappasoft\LaravelLivewireTables\Views\Columns\LinkColumn;
 use App\Models\Product;
 
 class ProductsTable extends DataTableComponent
@@ -44,6 +46,30 @@ class ProductsTable extends DataTableComponent
                 ->sortable(),
             Column::make("PuntosWebsite", "puntosWebsite")
                 ->sortable(),
+            ButtonGroupColumn::make('Acciones')
+                ->attributes(function($row){
+                    return [
+                        'class' => 'space-x-2',
+                    ];
+                })
+                ->buttons([
+                    LinkColumn::make('View')
+                        ->title(fn($row) => 'Editar')
+                        ->location(fn($row) => '#')
+                        ->attributes(function($row){
+                            return [
+                                'class' => 'px-3 py-2 btn bg-green-600 hover:btn h-12 w-32 bg-green-700 rounded-lg text-white opacity-100',
+                            ];
+                        }),
+                    LinkColumn::make('View')
+                        ->title(fn($row) => 'Detalles')
+                        ->location(fn($row) => '#')
+                        ->attributes(function($row){
+                            return [
+                                'class' => 'px-3 py-2 btn bg-blue-700 hover:btn h-12 w-32 bg-blue-800 rounded-lg text-white opacity-100',
+                            ];
+                        })
+                    ]),
         ];
     }
 }
